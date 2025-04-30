80f9a3519146d2fc0e6ff5014b2452ea484da682
dev-takhi77 <ewantomson1103@outlook.com>
2025-04-30 03:49:44 -0700

    fix: postbalance change

--- utils/getBalanceChange.ts
+++ utils/getBalanceChange.ts
@@ -5,12 +5,23 @@ import {
 export function balanceChanges(tx: VersionedTransactionResponse, mint: string, pool: string): number {
   if (!tx.meta) return 0;
   const { preTokenBalances, postTokenBalances } = tx.meta;
+
+  let preTokenBal = 0;
+  let postTokenBal = 0;
+
   for (let i = 0; i < preTokenBalances.length; i++) {
     const preTokenBalance = preTokenBalances[i];
-    const postTokenBalance = postTokenBalances[i];
     if (preTokenBalance.mint === mint && preTokenBalance.owner === pool) {
-      return Math.abs(postTokenBalance.uiTokenAmount.uiAmount - preTokenBalance.uiTokenAmount.uiAmount);
+      preTokenBal = preTokenBalance.uiTokenAmount.uiAmount;
     }
   }
-  return 0;
+
+  for (let i = 0; i < postTokenBalances.length; i++) {
+    const postTokenBalance = postTokenBalances[i];
+    if (postTokenBalance.mint === mint && postTokenBalance.owner === pool) {
+      postTokenBal = postTokenBalance.uiTokenAmount.uiAmount;
+    }
+  }
+
+  return Math.abs(postTokenBal - preTokenBal);
 }
